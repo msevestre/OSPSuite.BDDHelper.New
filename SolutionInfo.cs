bbf4aa20f31481cdecf9d1ec73766642045222e8
Michael Sevestre <michael@design2code.ca>
2019-05-11 11:27:37 -0400

    Add symboles

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Open Systems Pharmacology Community")]
 [assembly: AssemblyProduct("OSPSuite.BDDHelper")]
-[assembly: AssemblyCopyright("Copyright © 2002 - present - Open Systems Pharmacology Community")]
+[assembly: AssemblyCopyright("Copyright © 2017 - present - Open Systems Pharmacology Community")]
 [assembly: ComVisible(false)]
